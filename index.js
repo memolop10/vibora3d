40e4e2e65308b7ac95235979b119f291af6c689d
memolop10 <inglopezvargas@gmail.com>
2020-05-11 16:30:15 -0500

    juego snake completo

--- index.js
+++ index.js
@@ -1,111 +1,112 @@
-//variables globales
+//Variables globales
 var areaJ = document.querySelector("#areaJ");
 var esp = 40;
-
 //clases
-class Cabeza{
+class Cabeza {
     constructor(x,y){
         this.x = x;
         this.y = y;
         this.cuadros = new Array();
+        this.siguiente = null;
         this.construirCuerpo();
         this.asignarClases();
-        this.mover(this.x , this.y);
-        this.agregarElemento();
-    } 
-
+        this.mover(this.x,this.y);
+        this.agregarElementos();
+    }
     construirCuerpo(){
-
-        for(var i=0; i < 4 ;i++){
+        for(var i=0; i<4;i++){
             this.cuadros[i] = document.createElement("div");
         }
-
     }
-
     asignarClases(){
         this.cuadros[0].className = "snake";
         this.cuadros[1].className = "snake tapa";
         this.cuadros[2].className = "snake atras";
         this.cuadros[3].className = "snake alante";
     }
-
     mover(x,y){
-
-        this.x =x;
+        if(this.siguiente != null){
+            this.siguiente.mover(this.x,this.y);
+        }
+        this.x = x;
         this.y = y;
-
-        for (var i = 0; i < this.cuadros.length; i++) {
-            this.cuadros[i].style.left = this.x + "px" ;
-            this.cuadros[i].style.top = this.y + "px";
-            
+        for(var i=0; i<this.cuadros.length; i++){
+            this.cuadros[i].style.left = this.x+"px";
+            this.cuadros[i].style.top = this.y+"px";
         }
     }
+    agregar(){
+        if(this.siguiente == null){
+            this.siguiente = new Cabeza(this.x,this.y);
+        } else {
+            this.siguiente.agregar();
+        }
+    }
+    agregarElementos(){
+        for(var i=0; i<this.cuadros.length; i++){
+            areaJ.appendChild(this.cuadros[i]);
+        }
+    }
+    choqueBloque(otro){
+        var difx = Math.abs(this.x - otro.x);
+        var dify = Math.abs(this.y - otro.y);
+        if(difx < 40 && dify < 40){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    verSiguiente(){
+        return this.siguiente;
+    }
+}
 
-    agregarElemento(){
-        for (var i = 0; i < this.cuadros.length; i++) {
-            areaJ.appendChild(this.cuadros[i])
-            
+class Comida {
+    constructor(){
+        this.cuadros = new Array();
+        this.construirCuerpo();
+        this.asignarClases();
+        this.colocar();
+        this.agregarElementos();
+    }
+    construirCuerpo(){
+        for(var i=0; i<4;i++){
+            this.cuadros[i] = document.createElement("div");
+        }
+    }
+    asignarClases(){
+        this.cuadros[0].className = "snake comida";
+        this.cuadros[1].className = "snake tapa comida";
+        this.cuadros[2].className = "snake atras comida";
+        this.cuadros[3].className = "snake alante comida";
+    }
+    posAl(){
+        return (Math.round(Math.random() * 9))*40;
+    }
+    colocar(){
+        this.x = this.posAl();
+        this.y = this.posAl();
+        for(var i=0; i<this.cuadros.length; i++){
+            this.cuadros[i].style.left = this.x+"px";
+            this.cuadros[i].style.top = this.y+"px";
+        }
+    }
+    agregarElementos(){
+        for(var i=0; i<this.cuadros.length; i++){
+            areaJ.appendChild(this.cuadros[i]);
         }
     }
 }
 
-// class Comida{
-//     constructor(){
-//         this.cuadros = new Array();
-//         this.construirCuerpo();
-//         this.asignarClases();
-//         this.colocar();
-//         this.agregarElemento();
-//     }
-//     construirCuerpo(){
-
-//         for(var i = 0; i < 4;i++){
-//             this.cuadros[i] = document.createElement("div");
-//         }
-
-//     }
-
-//     asignarClases(){
-//         this.cuadros[0].className = "snake comida";
-//         this.cuadros[1].className = "snake tapa comida";
-//         this.cuadros[2].className = "snake atras comida";
-//         this.cuadros[3].className = "snake alante comida";
-
-//     }
-
-//     posAl(){
-//         return (Math.round(Math.random()*9))*40;
-//     }
-
-//     colocar(){
-//         this.x = this.posAl();
-//         this.y = this.posAl();
-
-
-//         for (var i = 0; i < this.cuadros.length; i++) {
-//             this.cuadros[i].style.left = this.x + "px" ;
-//             this.cuadros[i].style.top = this.y + "px";
-            
-//         }
-//     }
-
-//     agregarElemento(){
-//         for (var i = 0; i < this.cuadros.length; i++) {
-//             areaJ.appendChild(this.cuadros[i])
-            
-//         } 
-//     }
-// }
-
 //Objetos y variables del juego
 var snake = new Cabeza(0,0);
-var xdir = 0;
+var comida = new Comida();
+var xidr = 0;
 var ydir = 0;
 var ejex = true;
 var ejey = true;
-//motor de juego
-
-document.addEventListener("keydown",function(){
+//Motor juego
+document.addEventListener("keydown", function(){
     var cod = event.keyCode;
     if(ejex){
         if(cod == 38){
@@ -113,26 +114,21 @@ document.addEventListener("keydown",function(){
             ydir = -esp;
             ejex = false;
             ejey = true;
-
         }
         if(cod == 40){
             xdir = 0;
             ydir = esp;
             ejex = false;
             ejey = true;
-
         }
     }
-
     if(ejey){
-        //37 tecla izquierda
         if(cod == 37){
             xdir = -esp;
             ydir = 0;
             ejex = true;
             ejey = false;
         }
-        //39 tecla derecha
         if(cod == 39){
             xdir = esp;
             ydir = 0;
@@ -142,24 +138,39 @@ document.addEventListener("keydown",function(){
     }
 })
 function mover(){
-   var nx = snake.x + xdir;
-   var ny = snake.y + ydir; 
-    snake.mover(nx , ny)
-
+    var nx = snake.x + xdir;
+    var ny = snake.y + ydir;
+    snake.mover(nx,ny);
 }
-
 function colisiones(){
-
+    if(snake.choqueBloque(comida)){
+        snake.agregar();
+        comida.colocar();
+    }
+    if(snake.x < 0 || snake.x > 360 || snake.y < 0 || snake.y > 360){
+        findejuego();
+    }
+    var temp = null;
+    try{
+        temp = snake.verSiguiente().verSiguiente();
+    } catch(err){
+        temp = null;
+    }
+    while(temp != null){
+        if(snake.choqueBloque(temp)){
+            findejuego();
+            temp = null;
+        } else {
+            temp = temp.verSiguiente();
+        }
+    }
 }
-
 function findejuego(){
     alert("Perdiste");
     document.location.reload();
 }
-
 function bucle(){
     mover();
     colisiones();
 }
-
-setInterval("bucle()", 400)
+setInterval("bucle()", 500);
